a739bd97a34258adbfb076b8b477b8f754ff55c7
Jovieh August Mirembe. <joviahnabalamba@gmail.com>
2025-07-05 16:17:07 +0300

    Add files via upload

---
+++ PassengerSource1/src/main/java/joviah/passengersource1/Company.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package joviah.passengersource1;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Company {
+    private boolean alwaysSucceedsScheduling; 
+
+    /**
+     *
+     */
+    public Company() {
+        this.alwaysSucceedsScheduling = false; 
+    }
+
+   
+    public boolean scheduleVehicle(Passengersource.Passenger passenger) {
+      
+        System.out.println("Company attempting to schedule vehicle for Passenger: " + passenger.getId());
+        if (alwaysSucceedsScheduling) {
+            return true;
+        } else {
+           
+            return passenger.getSeatsRequired() <= 5; 
+        }
+    }
+
+    public void setAlwaysSucceedsScheduling(boolean alwaysSucceedsScheduling) {
+        this.alwaysSucceedsScheduling = alwaysSucceedsScheduling;
+    }
+}
+
+    
+
