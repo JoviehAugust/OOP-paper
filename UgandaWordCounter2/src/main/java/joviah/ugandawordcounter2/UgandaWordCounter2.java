b2acbd7f3b8c46db61071a69024a73f53c01dec4
Jovieh August Mirembe. <joviahnabalamba@gmail.com>
2025-07-05 14:52:31 +0300

    Add files via upload

---
+++ UgandaWordCounter2/src/main/java/joviah/ugandawordcounter2/UgandaWordCounter2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package joviah.ugandawordcounter2;
+
+/**
+ *
+ * @author ADMIN
+ */
+   import java.util.Scanner;
+public class UgandaWordCounter2 {
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter book description:");
+        String description = scanner.nextLine();
+
+        String lowerDesc = description.toLowerCase();
+        String target = "uganda";
+        int count = 0;
+        int index = 0;
+
+        while ((index = lowerDesc.indexOf(target, index)) != -1) {
+            count++;
+            index += target.length();
+        }
+
+        System.out.println("The word \"Uganda\" appears " + count + " times in the description.");
+
+        scanner.close();
+    }
+}
