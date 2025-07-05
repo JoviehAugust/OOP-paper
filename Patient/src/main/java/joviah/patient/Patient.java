05c3fb04470b9af5185b5cfffdbc96fb931b16cc
Jovieh August Mirembe. <joviahnabalamba@gmail.com>
2025-07-05 14:45:11 +0300

    Add files via upload

---
+++ Patient/src/main/java/joviah/patient/Patient.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package joviah.patient;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Patient {
+    private String id;
+    private String allergyNotes;
+
+    public Patient(String id, String allergyNotes) {
+        this.id = id;                 
+        this.allergyNotes = allergyNotes;  
+    }
+}
+
+
+
